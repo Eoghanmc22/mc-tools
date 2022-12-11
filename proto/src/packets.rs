b8a7f580ab9005a73ed4678fec03c099fe8e5f86
Eoghanmc22 <eoghanmc@icloud.com>
2022-12-10 22:08:34 -0500

    login packets

--- proto/src/packets.rs
+++ proto/src/packets.rs
@@ -1,7 +1,8 @@
 // TODO "Temporary" manual implemention of some packets
+// TODO Packet trait
 
-pub mod login {
-    use crate::{Data, DecodingError};
+pub mod handshake {
+    use crate::{primitive::varint::var_int, Data, DecodingError};
 
     pub struct Handshake<'a> {
         protocol_version: u32,
@@ -11,12 +12,12 @@ pub mod login {
     }
 
     impl<'a> Data<'a> for Handshake<'a> {
-        fn try_decode<'b: 'a>(_buffer: &'a mut &'b [u8]) -> Result<Handshake<'a>, DecodingError> {
+        fn try_decode<'b: 'a>(_buffer: &'a mut &'b [u8]) -> Result<Self, DecodingError> {
             todo!()
         }
 
         fn expected_size(&self) -> usize {
-            let mut len = 0;
+            let mut len = 1;
 
             len += self.protocol_version.expected_size();
             len += self.server_address.expected_size();
@@ -27,6 +28,7 @@ pub mod login {
         }
 
         fn encode<'b>(&self, buffer: &'b mut [u8]) -> &'b mut [u8] {
+            let buffer = var_int(0x00).encode(buffer);
             let buffer = self.protocol_version.encode(buffer);
             let buffer = self.server_address.encode(buffer);
             let buffer = self.server_port.encode(buffer);
@@ -35,3 +37,75 @@ pub mod login {
         }
     }
 }
+
+pub mod login {
+    use crate::{primitive::varint::var_int, Data, DecodingError};
+
+    pub struct LoginStart<'a> {
+        username: &'a str,
+    }
+
+    impl<'a> Data<'a> for LoginStart<'a> {
+        fn try_decode<'b: 'a>(_buffer: &'a mut &'b [u8]) -> Result<Self, DecodingError> {
+            todo!()
+        }
+
+        fn expected_size(&self) -> usize {
+            let mut len = 1;
+
+            len += self.username.expected_size();
+
+            len
+        }
+
+        fn encode<'b>(&self, buffer: &'b mut [u8]) -> &'b mut [u8] {
+            let buffer = var_int(0x00).encode(buffer);
+            let buffer = self.username.encode(buffer);
+            buffer
+        }
+    }
+
+    pub struct LoginSuccess<'a> {
+        uuid: u128,
+        username: &'a str,
+    }
+
+    impl<'a> Data<'a> for LoginSuccess<'a> {
+        fn try_decode<'b: 'a>(buffer: &'a mut &'b [u8]) -> Result<Self, DecodingError> {
+            Ok(LoginSuccess {
+                uuid: Data::try_decode(buffer)?,
+                username: Data::try_decode(buffer)?,
+            })
+        }
+
+        fn expected_size(&self) -> usize {
+            todo!()
+        }
+
+        fn encode<'b>(&self, _buffer: &'b mut [u8]) -> &'b mut [u8] {
+            todo!()
+        }
+    }
+
+    pub struct SetCompression {
+        threshold: u32,
+    }
+
+    impl<'a> Data<'a> for SetCompression {
+        fn try_decode<'b: 'a>(buffer: &'a mut &'b [u8]) -> Result<Self, DecodingError> {
+            Ok(SetCompression {
+                threshold: Data::try_decode(buffer)?,
+            })
+        }
+
+        fn expected_size(&self) -> usize {
+            todo!()
+        }
+
+        fn encode<'b>(&self, _buffer: &'b mut [u8]) -> &'b mut [u8] {
+            todo!()
+        }
+    }
+}
+
+pub mod play {}
